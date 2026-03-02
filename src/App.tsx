60b0e381f5a85ea60f5c47d819ecb740720491e2
Helen Tan <helentanmz0909@gmail.com>
2026-03-02 17:24:37 +0800

    test: test code for testing that theme is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import './App.css'
+import { Stack, Typography, Paper } from "@mui/material";
+import ThemeToggleButton from "./ThemeToggleButton";
 
-function App() {
-
-  return (<></>)
+export default function App() {
+  return (
+    <Stack spacing={2} alignItems="center" justifyContent="center" sx={{ p: 4 }}>
+      <Typography variant="h4">React + MUI Theme Test</Typography>
+      <Paper sx={{ p: 4, minWidth: 300 }}>
+        <Typography>
+          This is a Paper component. Its background and text should change with the theme.
+        </Typography>
+        <ThemeToggleButton />
+      </Paper>
+    </Stack>
+  );
 }
-
-export default App
