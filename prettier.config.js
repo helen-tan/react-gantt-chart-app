441a2e3cbb7c6270098bf5dcdd4f78d47a93c369
Helen Tan <helentanmz0909@gmail.com>
2026-03-06 00:02:06 +0800

    feat: add prettier rules

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  semi: true,              // add semicolons
+  singleQuote: true,       // use single quotes
+  trailingComma: "all",    // add trailing commas in objects, arrays, etc.
+  printWidth: 100,         // max line length
+  tabWidth: 2,             // indentation
+  bracketSpacing: true,    // { foo: bar } spacing
+  arrowParens: "always",   // always include parentheses in arrow functions
+  endOfLine: "lf",         // consistent line endings
+};
