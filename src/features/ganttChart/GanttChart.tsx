06c6eeb6fca9c86c6901472b411d74086a002a1c
Helen Tan <helentanmz0909@gmail.com>
2026-03-07 14:16:54 +0800

    feat: use mapping util methods in GanttChart

--- src/features/ganttChart/GanttChart.tsx
+++ src/features/ganttChart/GanttChart.tsx
@@ -6,15 +6,24 @@ import { ThemeModes } from '../../types/theme.model';
 import { Box } from '@mui/material';
 import { useGanttState } from './hooks/useGanttState';
 import './GanttChart.module.css';
+import {
+  maptoSVARGanttLinks,
+  maptoSVARGanttScales,
+  mapToSVARGanttTasks,
+} from './utils/mapToSVARGantt';
 
 export default function GanttChart() {
   const { mode } = useContext(ThemeContext);
   const { state } = useGanttState();
 
+  const tasks = useMemo(() => mapToSVARGanttTasks(state.tasks), []);
+  const links = useMemo(() => maptoSVARGanttLinks(state.links), []);
+  const scales = useMemo(() => maptoSVARGanttScales(state.scales), []);
+
   const renderLightTheme = useMemo(
     () => (
       <Willow>
-        <Gantt tasks={state.tasks} links={state.links} scales={state.scales} />
+        <Gantt tasks={tasks} links={links} scales={scales} />
       </Willow>
     ),
     [],
@@ -23,7 +32,7 @@ export default function GanttChart() {
   const renderDarkTheme = useMemo(
     () => (
       <WillowDark>
-        <Gantt tasks={state.tasks} links={state.links} scales={state.scales} />
+        <Gantt tasks={tasks} links={links} scales={scales} />
       </WillowDark>
     ),
     [],
