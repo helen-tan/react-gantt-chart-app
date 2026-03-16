d05bbe340379f6bb4bc390278c635e2ba2214315
Helen Tan <helentanmz0909@gmail.com>
2026-03-17 00:18:48 +0800

    feat: declare right drawer sizes

---
+++ src/contexts/rightDrawer/config.ts
@@ -0,0 +1,15 @@
+export const RightDrawerSizes = {
+  S: 'S',
+  M: 'M',
+  L: 'L',
+  XL: 'XL',
+};
+
+export type RightDrawerSize = (typeof RightDrawerSizes)[keyof typeof RightDrawerSizes];
+
+export const RIGHT_DRAWER_SIZES: Record<RightDrawerSize, number> = {
+  [RightDrawerSizes.S]: 300,
+  [RightDrawerSizes.M]: 400,
+  [RightDrawerSizes.L]: 600,
+  [RightDrawerSizes.XL]: 800,
+};
