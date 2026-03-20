3bcd937bb05dc7b7fde52d0bb38fc80ee1995de8
Helen Tan <helentanmz0909@gmail.com>
2026-03-21 01:42:27 +0800

    feat: added TASK_TYPE_OPTIONS for type dropdown

--- src/features/forms/createTaskForm/CreateTaskFormConfig.ts
+++ src/features/forms/createTaskForm/CreateTaskFormConfig.ts
@@ -1,3 +1,6 @@
+import type { FormSelectOption } from '../../../components/forms/FormSelect';
+import type { AppTaskType } from '../../ganttChart/types';
+
 export const CreateTaskFormFields = {
   TITLE: 'title',
   START: 'start',
@@ -11,3 +14,9 @@ export interface CreateTaskFormValues {
   [CreateTaskFormFields.END]: number;
   [CreateTaskFormFields.TYPE]: string;
 }
+
+export const TASK_TYPE_OPTIONS: FormSelectOption<AppTaskType>[] = [
+  { value: 'task', label: 'Task' },
+  { value: 'milestone', label: 'Milstone' },
+  { value: 'summary', label: 'Summary' },
+];
