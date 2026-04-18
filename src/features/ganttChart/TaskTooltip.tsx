6ab1691a4398f0ab9a93f60c7ff429f9e7345599
Helen Tan <helentanmz0909@gmail.com>
2026-04-19 00:16:44 +0800

    feat: Custom task tooltip component

---
+++ src/features/ganttChart/TaskTooltip.tsx
@@ -0,0 +1,69 @@
+import { Paper, Typography, useTheme } from '@mui/material';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
+import type { ITask } from '@svar-ui/react-gantt';
+import type { AppTask } from '../../contexts/gantt/types';
+import { useEffect, useMemo } from 'react';
+
+export type GanttTooltipData = {
+  data: ITask;
+};
+
+type TaskTooltipProps = {
+  data: ITask;
+};
+
+type ToolTipTaskData = {
+  title: string;
+  type: string;
+  start: string;
+  end: string;
+};
+
+export default function TaskTooltip({ data }: TaskTooltipProps) {
+  const theme = useTheme();
+
+  if (!data) return null;
+
+  const task: ToolTipTaskData = useMemo(
+    () => ({
+      title: data.text ?? '-',
+      type: data.type ?? '-',
+      start: data.start ? data.start.toLocaleDateString() : new Date().toLocaleDateString(),
+      end: data.end ? data.end.toLocaleDateString() : new Date().toLocaleDateString(),
+    }),
+    [],
+  );
+
+  return (
+    <Paper
+      elevation={0}
+      sx={{
+        width: 180,
+        backgroundColor: theme.palette.background.paper,
+        color: theme.palette.text.primary,
+        p: 1.5,
+        borderRadius: 3,
+        boxShadow: 'none',
+      }}
+    >
+      <Stack spacing={1}>
+        <Typography fontWeight={700}>{task.title}</Typography>
+        <Divider sx={{ color: theme.palette.text.secondary }} />
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant="body2" fontWeight={500}>
+            Start:
+          </Typography>
+          <Typography variant="body2">{task.start}</Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant="body2" fontWeight={500}>
+            End:
+          </Typography>
+          <Typography variant="body2">{task.end}</Typography>
+        </Box>
+      </Stack>
+    </Paper>
+  );
+}
